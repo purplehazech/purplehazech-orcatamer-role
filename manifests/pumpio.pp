7a0b471bdc32721157a893a2857a5f0bb6a06134
Lucas Bickel <hairmare@purplehaze.ch>
2014-03-17 22:24:08 +0100

    add mongodb profile

--- manifests/pumpio.pp
+++ manifests/pumpio.pp
@@ -3,9 +3,11 @@
 class role::pumpio {
   include ::profile::system
   include ::profile::nodejs
+  include ::profile::mongodb
   include ::profile::pumpio
 
   Class['::profile::system'] ->
   Class['::profile::nodejs'] ->
+  Class['::profile::mongodb'] ->
   Class['::profile::pumpio']
 }
